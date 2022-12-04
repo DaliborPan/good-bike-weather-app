779cf2f5b073357a8a94219b380a86fd583797ad
DaliborPan <pantlik.dalibor@seznam.cz>
2022-12-04 12:07:50 +0100

    Removed unused fetchers

--- good-bike-weather-app/src/services/weather.ts
+++ good-bike-weather-app/src/services/weather.ts
@@ -6,17 +6,6 @@ const WEATHER_TEMPERATURE_API_BASE = process.env.NEXT_PUBLIC_WEATHER_TEMPERATURE
 const WEATHER_PRECIPITATION_API_KEY = process.env.NEXT_PUBLIC_WEATHER_PRECIPITATION_API_KEY
 const WEATHER_TEMPERATURE_API_KEY = process.env.NEXT_PUBLIC_WEATHER_TEMPERATURE_API_KEY
 
-const getWeatherQuery = (year: number) => `?q={%22year%22:%20${year}}&max=15&skip=0`
-
-export const getPrecipitation = async (year: number): Promise<WeatherPrecipitationResponse> => {
-  const response = await axios.get(`${WEATHER_PRECIPITATION_API_BASE}precipitation${getWeatherQuery(year)}`, {
-    headers: {
-      'x-apikey': WEATHER_PRECIPITATION_API_KEY as string,
-    },
-  })
-  return response.data
-}
-
 export const getPrecipitationAllTime = async (): Promise<WeatherPrecipitationResponse> => {
   const response = await axios.get(`${WEATHER_PRECIPITATION_API_BASE}precipitation?q={}`, {
     headers: {
@@ -33,14 +22,5 @@ export const getTemperatureAllTime = async (): Promise<WeatherTemperatureRespons
     },
   })
 
-  return response.data
-}
-
-export const getTemperature = async (year: number): Promise<WeatherTemperatureResponse> => {
-  const response = await axios.get(`${WEATHER_TEMPERATURE_API_BASE}temp-avg${getWeatherQuery(year)}`, {
-    headers: {
-      'x-apikey': WEATHER_TEMPERATURE_API_KEY as string,
-    },
-  })
   return response.data
 }
